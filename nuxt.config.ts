e8fc29a4a3b2cedc428622f159b7abbdb01acf5e
Zokiasu <pierrick.tly@gmail.com>
2024-09-15 12:39:36 +0200

    Add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   components: true,
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss']
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'To-Do List\'s Pierrick',
+      meta: [
+        { name: 'description', content: 'To-Do List' },
+        { name: 'keywords', content: 'tâches, todo list, vuejs, nuxtjs' },
+        { name: 'author', content: 'Pierrick TAÏLY' },
+      ],
+    },
+  },
 })
